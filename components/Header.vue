3d6371fc755f318ae05e427360060b6b74dc48b8
xexuline <jscentella@gmail.com>
2019-01-27 17:56:00 +0100

    init

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="nav nav__fixed nav__fixed-top">
+    <ul>
+      <li class="nav__item">
+        <nuxt-link to="/">Curratelo.es</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/realtime">Realtime</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  .nav {
+    color: red;
+    background-color: #009688;
+    color: rgba(255,255,255,.84);
+    width: 100vw;
+    height: 4rem;  
+    display: flex;
+
+    &__fixed{
+      position: fixed;
+      z-index: 1;
+      &-top{
+        top:0;
+        left: 0;
+      }
+    }
+    &__item{
+      color: blue
+    }
+  }
+</style>
---
+++ pages/realtime.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <Header />
+  </section>
+</template>
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style lang="sass" scoped>
+
+</style>
